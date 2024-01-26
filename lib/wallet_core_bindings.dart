680269545f311df09ea8d5dbf9245b8f67b7fbcc
xuelongqy <xuelongqy@qq.com>
2024-01-26 14:46:33 +0000

    feat(TWTHORChainSwap): Add bindings.

--- lib/wallet_core_bindings.dart
+++ lib/wallet_core_bindings.dart
@@ -75,3 +75,4 @@ part 'src/tw_stored_key_encryption.dart';
 part 'src/tw_stored_key_encryption_level.dart';
 part 'src/tw_string.dart';
 part 'src/tw_tezos_message_signer.dart';
+part 'src/tw_thor_chain_swap.dart';
