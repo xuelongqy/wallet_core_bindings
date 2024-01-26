fe9168bbec10e68f119e0973fd5334095569ba77
xuelongqy <xuelongqy@qq.com>
2024-01-26 17:43:47 +0800

    feat(TWLiquidStaking): Add bindings.

---
+++ lib/src/tw_liquid_staking.dart
@@ -0,0 +1,14 @@
+part of '../wallet_core_bindings.dart';
+
+/// THORChain swap functions
+class TWLiquidStaking {
+  /// Builds a LiquidStaking transaction input.
+  ///
+  /// \param [input] The serialized data of LiquidStakingInput.
+  /// \return The serialized data of LiquidStakingOutput.
+  static Uint8List buildRequest(Uint8List input) => TWData.fromPointer(
+        iTWBindings.TWLiquidStakingBuildRequest(
+          TWData(input).pointer,
+        ),
+      ).bytes()!;
+}
