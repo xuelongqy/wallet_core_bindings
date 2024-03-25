aed646a7ff8a49163dcfc05b68ca0337d04cee19
xuelongqy <xuelongqy@qq.com>
2024-03-25 15:11:47 +0800

    test(Boba): Add test cases.

---
+++ test/chains/Boba/tw_coin_type_test.dart
@@ -0,0 +1,30 @@
+import 'package:test/test.dart';
+import 'package:wallet_core_bindings/wallet_core_bindings.dart';
+
+void main() {
+  group('TWBobaCoinType', () {
+    test('TWCoinType', () {
+      const coin = TWCoinType.TWCoinTypeBoba;
+      final symbol = TWCoinTypeConfiguration.getSymbol(coin);
+      final id = TWCoinTypeConfiguration.getID(coin);
+      final name = TWCoinTypeConfiguration.getName(coin);
+      const txId =
+          '0x31533707c3feb3b10f7deeea387ff8893f229253e65ca6b14d2400bf95b5d103';
+      final txUrl = TWCoinTypeConfiguration.getTransactionURL(coin, txId);
+      const accId = '0x4F96F50eDB37a19216d87693E5dB241e31bD3735';
+      final accUrl = TWCoinTypeConfiguration.getAccountURL(coin, accId);
+
+      expect(id, 'boba');
+      expect(name, 'Boba');
+      expect(symbol, 'BOBAETH');
+      expect(TWCoinTypeConfiguration.getDecimals(coin), 18);
+      expect(TWCoinTypeBlockchain(coin), TWBlockchain.TWBlockchainEthereum);
+      expect(TWCoinTypeP2shPrefix(coin), 0x0);
+      expect(TWCoinTypeStaticPrefix(coin), 0x0);
+      expect(txUrl,
+          'https://blockexplorer.boba.network/tx/0x31533707c3feb3b10f7deeea387ff8893f229253e65ca6b14d2400bf95b5d103');
+      expect(accUrl,
+          'https://blockexplorer.boba.network/address/0x4F96F50eDB37a19216d87693E5dB241e31bD3735');
+    });
+  });
+}
