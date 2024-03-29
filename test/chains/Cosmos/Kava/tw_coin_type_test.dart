9dcac068fbf0f86ef2038513172ca71868292461
xuelongqy <xuelongqy@qq.com>
2024-03-29 12:14:01 +0800

    test(Kava): Add test cases.

---
+++ test/chains/Cosmos/Kava/tw_coin_type_test.dart
@@ -0,0 +1,32 @@
+import 'package:test/test.dart';
+import 'package:wallet_core_bindings/wallet_core_bindings.dart';
+
+void main() {
+  group('TWKavaCoinType', () {
+    test('TWCoinType', () {
+      const coin = TWCoinType.TWCoinTypeKava;
+      final symbol = TWCoinTypeConfiguration.getSymbol(coin);
+      final id = TWCoinTypeConfiguration.getID(coin);
+      final name = TWCoinTypeConfiguration.getName(coin);
+      const txId =
+          '2988DF83FCBFAA38179D583A96734CBD071541D6768221BB23111BC8136D5E6A';
+      final txUrl = TWCoinTypeConfiguration.getTransactionURL(coin, txId);
+      const accId = 'kava1xd39avn2f008jmvua0eupg39zsp2xn3wf802vn';
+      final accUrl = TWCoinTypeConfiguration.getAccountURL(coin, accId);
+      final chainId = TWCoinTypeChainId(coin);
+
+      expect(id, 'kava');
+      expect(name, 'Kava');
+      expect(symbol, 'KAVA');
+      expect(TWCoinTypeConfiguration.getDecimals(coin), 6);
+      expect(TWCoinTypeBlockchain(coin), TWBlockchain.TWBlockchainCosmos);
+      expect(TWCoinTypeP2shPrefix(coin), 0x0);
+      expect(TWCoinTypeStaticPrefix(coin), 0x0);
+      expect(chainId, "kava_2222-10");
+      expect(txUrl,
+          'https://mintscan.io/kava/txs/2988DF83FCBFAA38179D583A96734CBD071541D6768221BB23111BC8136D5E6A');
+      expect(accUrl,
+          'https://mintscan.io/kava/account/kava1xd39avn2f008jmvua0eupg39zsp2xn3wf802vn');
+    });
+  });
+}
