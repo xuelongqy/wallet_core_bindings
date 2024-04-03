36d02a5961286c59a077a702ed8428183dffb618
xuelongqy <xuelongqy@qq.com>
2024-04-03 17:59:46 +0800

    test(ECO): Add test cases.

---
+++ test/chains/ECO/tw_coin_type_test.dart
@@ -0,0 +1,28 @@
+import 'package:test/test.dart';
+import 'package:wallet_core_bindings/wallet_core_bindings.dart';
+
+void main() {
+  group('TWHECOCoinType', () {
+    test('TWCoinType', () {
+      const coin = TWCoinType.TWCoinTypeECOChain;
+      final symbol = TWCoinTypeConfiguration.getSymbol(coin);
+      final id = TWCoinTypeConfiguration.getID(coin);
+      final name = TWCoinTypeConfiguration.getName(coin);
+      const txId =
+          '0x2e62832615f5b68b3bbcd72046a24260ce47052841c1679841b9c574d3959f13';
+      final txUrl = TWCoinTypeConfiguration.getTransactionURL(coin, txId);
+      const accId = '0xc5a5b3e49e5d06afe163553c942dc59b4e358cf1';
+      final accUrl = TWCoinTypeConfiguration.getAccountURL(coin, accId);
+
+      expect(id, 'heco');
+      expect(name, 'Huobi ECO Chain');
+      expect(symbol, 'HT');
+      expect(TWCoinTypeConfiguration.getDecimals(coin), 18);
+      expect(TWCoinTypeBlockchain(coin), TWBlockchain.TWBlockchainEthereum);
+      expect(txUrl,
+          'https://hecoinfo.com/tx/0x2e62832615f5b68b3bbcd72046a24260ce47052841c1679841b9c574d3959f13');
+      expect(accUrl,
+          'https://hecoinfo.com/address/0xc5a5b3e49e5d06afe163553c942dc59b4e358cf1');
+    });
+  });
+}
