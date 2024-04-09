a09ed5f6099404034d7fc2b0b7ac06afacafd351
xuelongqy <xuelongqy@qq.com>
2024-04-09 16:03:49 +0800

    test(Ethereum): Add test cases.

---
+++ test/chains/Ethereum/address_test.dart
@@ -0,0 +1,52 @@
+import 'package:test/test.dart';
+import 'package:wallet_core_bindings/wallet_core_bindings.dart';
+
+void main() {
+  group('EthereumAddress', () {
+    const coin = TWCoinType.TWCoinTypeEthereum;
+
+    test('Invalid', () {
+      expect(TWAnyAddress.isValid('abc', coin), false);
+      expect(
+        TWAnyAddress.isValid('aaeb60f3e94c9b9a09f33669435e7ef1beaed', coin),
+        false,
+      );
+      expect(
+        TWAnyAddress.isValid('fB6916095ca1df60bB79Ce92cE3Ea74c37c5d359', coin),
+        false,
+      );
+    });
+
+    test('String', () {
+      expect(
+        TWAnyAddress('0x5aaeb6053f3e94c9b9a09f33669435e7ef1beaed', coin)
+            .description,
+        '0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed',
+      );
+    });
+
+    test('FromPrivateKey', () {
+      final privateKey = TWPrivateKey.createWithHexString(
+          'afeefca74d9a325cf1d6b6911d61a65c32afa8e02bd5e78e2e4ac2910bab45f5');
+      final publicKey = privateKey
+          .getPublicKeyByType(TWPublicKeyType.TWPublicKeyTypeSECP256k1Extended);
+      final address = TWAnyAddress.createWithPublicKey(publicKey, coin);
+
+      expect(address.description, '0xAc1ec44E4f0ca7D172B7803f6836De87Fb72b309');
+
+      final publicKey2 =
+          privateKey.getPublicKeyByType(TWPublicKeyType.TWPublicKeyTypeED25519);
+      final address2 = TWAnyAddress.createWithPublicKey(publicKey2, coin);
+      expect(address2.description, '');
+    });
+
+    test('IsValid', () {
+      expect(TWAnyAddress.isValid('abc', coin), false);
+      expect(
+        TWAnyAddress.isValid(
+            '0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed', coin),
+        true,
+      );
+    });
+  });
+}
