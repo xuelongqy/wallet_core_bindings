79d226239a45f4a4508fbb1f1074c5375bab7152
xuelongqy <xuelongqy@qq.com>
2024-04-18 19:06:09 +0800

    test(NEO): Add test cases.

---
+++ test/chains/NEO/tw_coin_type_test.dart
@@ -0,0 +1,30 @@
+import 'package:test/test.dart';
+import 'package:wallet_core_bindings/wallet_core_bindings.dart';
+
+void main() {
+  group('TWNEOCoinType', () {
+    test('TWCoinType', () {
+      const coin = TWCoinType.TWCoinTypeNEO;
+      final symbol = TWCoinTypeConfiguration.getSymbol(coin);
+      final id = TWCoinTypeConfiguration.getID(coin);
+      final name = TWCoinTypeConfiguration.getName(coin);
+      const txId =
+          'e0ddf7c81c732df26180aca0c36d5868ad009fdbbe6e7a56ebafc14bba41cd53';
+      final txUrl = TWCoinTypeConfiguration.getTransactionURL(coin, txId);
+      const accId = 'AcxuqWhTureEQGeJgbmtSWNAtssjMLU7pb';
+      final accUrl = TWCoinTypeConfiguration.getAccountURL(coin, accId);
+
+      expect(id, 'neo');
+      expect(name, 'NEO');
+      expect(symbol, 'NEO');
+      expect(TWCoinTypeConfiguration.getDecimals(coin), 8);
+      expect(TWCoinTypeBlockchain(coin), TWBlockchain.TWBlockchainNEO);
+      expect(TWCoinTypeP2shPrefix(coin), 0x0);
+      expect(TWCoinTypeStaticPrefix(coin), 0x0);
+      expect(txUrl,
+          'https://neoscan.io/transaction/e0ddf7c81c732df26180aca0c36d5868ad009fdbbe6e7a56ebafc14bba41cd53');
+      expect(accUrl,
+          'https://neoscan.io/address/AcxuqWhTureEQGeJgbmtSWNAtssjMLU7pb');
+    });
+  });
+}
