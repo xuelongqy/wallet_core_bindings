c013a07ebc53a934fc768c5480dea7f3d8564873
xuelongqy <xuelongqy@qq.com>
2024-12-08 22:00:24 +0800

    update(TrustWalletCore): 4.1.21

---
+++ wallet_core_bindings_libs/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import wallet_core_bindings_libs
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  WalletCoreBindingsLibsPlugin.register(with: registry.registrar(forPlugin: "WalletCoreBindingsLibsPlugin"))
+}
